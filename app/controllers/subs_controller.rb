b5f81f4fc54d5284bdd0877dc3201cbe0e0decf0
Callice Austin <callice.a@gmail.com>
2021-05-19 11:18:08 -0600

    show/delete

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,20 +1,34 @@
 class SubsController < ApplicationController
+    before_action :set_sub, only: [:show, :update, :edit, :destroy]
 
     def index
-        render component: "Subs"
+        @subs = Sub.all
+        render component: "Subs", props: {subs: @subs}
     end
 
     def show
-        render component: "Sub"
+        # @sub = Sub.find(params[:id])
+        render component: "Sub", props: {sub: @sub}
     end
     def edit
+        # @sub = Sub.find(params[:id])
         render component: "SubEdit"
     end
     def new
         render component: "SubNew"
     end
-    def delete
-        
+    def update
+        @sub = Sub.find(params[:id])
+    end
+    def destroy
+        # @sub = Sub.find(params[:id])
+        @sub.destroy
+        redirect_to root_path
+    end
+
+    private
+    def set_sub
+        @sub = Sub.find(params[:id])
     end
 
 end
--- app/javascript/components/Subs.js
+++ app/javascript/components/Subs.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Subs = (props) => {
+    const {subs} = props
+    const renderSubs = ()=> {
+
+        return subs.map(sub => {
+            return(
+                <div>
+                    <h1>{sub.name}</h1>
+                    <a href={`/subs/${sub.id}`}>Show</a>
+                    <a href={`/subs/${sub.id}/edit`}>Edit</a>
+                    <a href={`/subs/${sub.id}`} data-method='delete'>Delete</a>
+                </div>
+            )
+        })
+    }
     return(
         <div>
-            <h1>Subs component</h1>
+            <h1>{renderSubs()}</h1>
         </div>
     )
 }
