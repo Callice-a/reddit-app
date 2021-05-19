f12762864d0d82cc777ec8eeacc6bb169ec08a45
Callice Austin <callice.a@gmail.com>
2021-05-19 10:29:05 -0600

    setup routes

---
+++ app/javascript/components/SubEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubEdit = (props) => {
+    return(
+        <div>
+            <h1>Subs edit component</h1>
+        </div>
+    )
+}
+
+export default SubEdit
---
+++ app/javascript/components/SubNew.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubNew = (props) => {
+    return(
+        <div>
+            <h1>SubNew component</h1>
+        </div>
+    )
+}
+
+export default SubNew
